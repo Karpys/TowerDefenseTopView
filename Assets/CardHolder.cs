541f459dcad20c22eab3385130787e5fc966cdb6
Remi Vedrenne <remi.vedrenne@edu.devinci.fr>
2021-01-23 02:12:35 +0100

    [Feat] Card Construct

--- Assets/CardHolder.cs
+++ Assets/CardHolder.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
+
 
 public class CardHolder : MonoBehaviour
 {
     // Start is called before the first frame update
     public CardStats stats;
     public Image Image;
+    public TextMeshProUGUI Nom;
+    public TextMeshProUGUI Description;
+   
     void Start()
     {
         Image.sprite = stats.Image;
+        Nom.text = stats.name;
+        Description.text = stats.description;
     }
 
     // Update is called once per frame
--- Assets/CardStats.cs
+++ Assets/CardStats.cs
@@ -10,5 +10,6 @@ public class CardStats : ScriptableObject
     // Start is called before the first frame update
     public Sprite Image;
     public new string name;
+    public string description;
     public int cost;
 }
