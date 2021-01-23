2b9cf4b1926884f5ddf8e34412a65151f235728b
Remi Vedrenne <remi.vedrenne@edu.devinci.fr>
2021-01-23 01:40:23 +0100

    [Feat] Start Work on Card System

---
+++ Assets/CardHolder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CardHolder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CardStats stats;
+    public Image Image;
+    void Start()
+    {
+        Image.sprite = stats.Image;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/CardStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Object", menuName = "CardStats")]
+
+public class CardStats : ScriptableObject
+{
+    // Start is called before the first frame update
+    public Sprite Image;
+    public new string name;
+    public int cost;
+}
