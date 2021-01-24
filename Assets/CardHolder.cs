c6c3914a90f96925472b511045ea10e04bb050aa
Remi Vedrenne <remi.vedrenne@edu.devinci.fr>
2021-01-24 13:05:51 +0100

    [Feat] Drag And Drop Card System

--- Assets/CardHolder.cs
+++ Assets/CardHolder.cs
@@ -13,18 +13,51 @@ public class CardHolder : MonoBehaviour
     public TextMeshProUGUI Nom;
     public TextMeshProUGUI Description;
     public TextMeshProUGUI Cost;
-   
+    public GameObject Descri;
+
+
+
+    public Vector3 startPos;
+    public Vector3 PostoGo;
+    public bool MouseOn;
+    public float UpDiff;
+    public float speed;
     void Start()
     {
         Image.sprite = stats.Image;
         Nom.text = stats.name;
         Description.text = stats.description;
         Cost.text = stats.cost.ToString();
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.Lerp(transform.position, PostoGo, speed);
+        if(Input.GetMouseButton(0) && MouseOn)
+        {
+            transform.position = Mouse.MousePosition;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Mouse"))
+        {
+            PostoGo = new Vector3(startPos.x, startPos.y + UpDiff, startPos.z);
+            MouseOn = true;
+            Descri.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Mouse"))
+        {
+            PostoGo = startPos;
+            MouseOn = false;
+            Descri.SetActive(false);
+        }
     }
 }
