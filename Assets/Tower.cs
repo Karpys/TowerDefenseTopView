dfd23a69895da909ee8ded47c8812a384547d4b3
Remi Vedrenne <remi.vedrenne@edu.devinci.fr>
2021-01-27 00:16:45 +0100

    [Feat] FIrst Tower Commit and Add Circle Range System

---
+++ Assets/Tower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static MathsUti.MathUtilities;
+public class Tower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float range;
+    public GameObject Circle;
+    public float multi;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Circle Range
+        multi=2/Circle.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        Circle.transform.localScale = new Vector3(range*multi, range * multi, 1);
+    }
+}
