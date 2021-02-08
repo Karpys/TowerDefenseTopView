e50e9f03382096f9d6b6e359d853912dc871e93c
Remi Vedrenne <remi.vedrenne@edu.devinci.fr>
2021-02-08 23:36:32 +0100

    [Feat] Dps Check Scene

---
+++ Assets/DpsCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DpsCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Ennemy Ennemy;
+    public float DivideDps;
+    public float lifestart;
+    public float Dps;
+    public TextMeshProUGUI Text;
+    void Start()
+    {
+        lifestart = Ennemy.life;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lifestart!=Ennemy.life)
+        {
+            DivideDps += Time.deltaTime;
+            Dps = (lifestart - Ennemy.life) / DivideDps;
+            Text.text = Dps.ToString();
+        }
+    }
+}
