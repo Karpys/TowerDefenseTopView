0c55a11872a5d72fff423b9ae9a250b91346be32
Remi Vedrenne <remi.vedrenne@edu.devinci.fr>
2021-02-14 21:36:48 +0100

    [Feat] Slash Freeze Anim and Spawn System start

---
+++ Assets/SpawnerSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject SpawnEnnemies;
+    public MapManager Map;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            GameObject En = Instantiate(SpawnEnnemies, transform.position, transform.rotation);
+            En.GetComponent<Ennemy>().Pathing = Map;
+            Map.ListEnnemy.Add(En);
+        }
+    }
+}
